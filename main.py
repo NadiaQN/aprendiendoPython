7dc92438260c254026781f70c66b464daf5b2401
Nadia Quezada <quezada.nadia@gmail.com>
2020-02-26 21:52:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import modulo as mod
+mod.saludar("Nadia")
+
+#parametro "rt":accion:read-modo:text
+archivo = open("ejemplo.txt", "rt")
+datos = archivo.read()
+
+print(datos)
---
+++ modulo.py
@@ -0,0 +1,2 @@
+def saludar(nombre):
+  print(f"Hola {nombre}, soy un modulo")
