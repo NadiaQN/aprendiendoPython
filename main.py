df2065ff89c228be6d06426f72650fcf5c758b6f
Nadia Quezada <quezada.nadia@gmail.com>
2020-02-26 22:02:45 +0000

    escribiendo archivos de texto

--- main.py
+++ main.py
@@ -4,5 +4,20 @@ mod.saludar("Nadia")
 #parametro "rt":accion:read-modo:text
 archivo = open("ejemplo.txt", "rt")
 datos = archivo.read()
+archivo.close()
 
 print(datos)
+
+archivo2 = open("ejemplo2.txt", "wt")
+texto = "Hola como están?!"
+archivo2.write(texto)
+archivo2.close()
+
+archivo3 = open("ejemplo2.txt", "rt")
+datos2 = archivo3.read()
+
+print(datos2)
+
+
+
+
